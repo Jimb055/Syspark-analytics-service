2fe806cb3b436acbe699fe2f11be3bbba3f341c0
jimboss <salazar.dj100@gmail.com>
2025-07-16 00:54:49 -0500

    feat: add schema StatisticsResponse  API responses

--- app/models/statistics.py
+++ app/models/statistics.py
@@ -1,9 +1,20 @@
 from sqlalchemy import Column, Integer, String
 from app.database import Base
+from pydantic import BaseModel
 
+# SQLAlchemy model (Database)
 class Statistics(Base):
     __tablename__ = "statistics"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
     value = Column(Integer, nullable=False)
+
+# Pydantic model (Response schema)
+class StatisticsResponse(BaseModel):
+    id: int
+    name: str
+    value: int
+
+    class Config:
+        orm_mode = True
